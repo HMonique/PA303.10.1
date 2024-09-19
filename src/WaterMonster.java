8813417d4292886f543f09cf038d0cd6c3f71b70
HMonique <moniquehl@outlook.com>
2024-09-19 14:22:10 -0400

    first commit

---
+++ src/WaterMonster.java
@@ -0,0 +1,8 @@
+public class WaterMonster extends Monster{
+    public WaterMonster(String name) {
+        super(name);
+    }
+    public String attack() {
+        return "Attack with water!";
+    }
+}
